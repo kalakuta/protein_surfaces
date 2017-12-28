7334a8820ad2d004a60619c9a807b2cfcdcd3c8b
kalakuta <patrick.macmahon@gmail.com>
2017-12-28 15:19:10 +0000

    updated rotation method

--- map_generation/spherical_test.py
+++ map_generation/spherical_test.py
@@ -1,7 +1,13 @@
 import random
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 
 def random_spherical():
+
     phi = random.random()*np.pi
     theta = random.random() * 2 * np.pi
     x, y, z = np.sin(phi) * np.cos(theta), np.sin(phi) * np.sin(theta), np.cos(phi)
@@ -16,11 +22,37 @@ def spherical_deflection(p1, p2, p3):
         sense = -1
     return(angle * sense)
 
+def rotation_matrix(axis, theta):
+    
+    # Use the Euler-Rodrigues formula to establish a rotation of theta radians about axis
+    
+    axis = np.asarray(axis)
+    theta = np.asarray(theta)
+    axis = axis/np.sqrt(np.dot(axis, axis))
+    a = np.cos(theta/2)
+    b, c, d = -axis*np.sin(theta/2)
+    aa, bb, cc, dd = a*a, b*b, c*c, d*d
+    bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+    return np.array([[aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac)],
+                     [2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab)],
+                     [2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc]])
+
+def rotate_onto_z(vector):
+	if vector[0] == 0 and vector[1] == 0:
+		return np.identity(3)
+	else:
+		#gives the rotation matrix to rotate a given vector onto [0, 0, 1]
+		axis = [vector[1],-vector[0],0]			# vector (a,-b,0) is perpendicular to (a,b,c) and to (0,0,1)
+		theta = np.arccos(vector[2] / np.sqrt(vector[0]*vector[0] + vector[1]*vector[1] + vector[2]*vector[2]))
+		return rotation_matrix(axis, theta)
+
+
 
 points = [[0., 0., -1.]]
 for j in range(10):
 	points.append(random_spherical())
 path_points = np.empty((0, 3))
+path_points = np.vstack([path_points, [-1., 0., 0.]])
 current_point = [0., 0., -1.]
 while len(points) > 0:
     path_points = np.vstack([path_points, current_point])
@@ -35,27 +67,83 @@ while len(points) > 0:
     current_point = min_point
 
 print(path_points)
-        
+
+# get 2d coordinates
+coords = np.empty((0, 2))
+current = np.asarray([0., 0.])
+angle = 0.
+for i in range(10):
+    coords = np.vstack([coords, current])
+    vector = path_points[i + 2, :] - path_points[i + 1, :]
+    length = np.sqrt(np.dot(vector, vector))
+    angle += spherical_deflection(path_points[i], path_points[i + 1], path_points[i + 2])
+    current += np.asarray([length * np.cos(angle), length * np.sin(angle)])
+coords = np.vstack([coords, current])
+print(coords)
     
 
-'''
+x = coords[:, 0]
+y = coords[:, 1]
 
-if len(path) == 1:
-	geo_coords = np.vstack([geo_coords, [0., 0.]])
-else:
-	coord = np.asarray([0., 0.])
-	for j in range(len(path) - 1):
-		path_points = path_points - surface[path[j]]
-		r = rotate_onto_z(direction[path[j]])
-		path_directions = np.transpose(np.dot(r, np.transpose(path_directions)))
-		g = geodesic_distances[path[j]][path[j+1]]
-		euc = np.sqrt(np.dot(path_points[j+1], path_points[j+1]))
-		coord += (g / euc) * np.asarray([path_points[j+1][0],path_points[j+1][1]])
-	geo_coords = np.vstack([geo_coords, [coord]])
-print(geo_coords)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(111)
+ax1.set_xlim(-5,5)
+ax1.set_ylim(-5,5)
+ax1.scatter(x, y)
+ax1.plot(x, y)
+#fig1.show()
+fig1.savefig("2Dpath.png")
 
-'''
+xs = path_points[1:, 0]
+ys = path_points[1:, 1]
+zs = path_points[1:, 2]
 
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(111, projection='3d')
+ax2.scatter(xs, ys, zs)
+ax2.plot(xs, ys, zs)
+u = np.linspace(0, 2 * np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+x = np.outer(np.cos(u), np.sin(v))
+y = np.outer(np.sin(u), np.sin(v))
+z = np.outer(np.ones(np.size(u)), np.cos(v))
+
+# Plot the surface
+ax2.plot_surface(x, y, z, color='#FFE6FF')
+
+fig2.savefig("3Dpath.png")
+
+
+path_points = path_points[1:,:]
+normals = - path_points
+
+coords2 = np.empty((0,2))
+#current_point = [0., 0.]
+coord = [0., 0.]
+coords2 = np.vstack([coords2, coord])
+for j in range(10):
+	path_points = path_points - path_points[j]
+	r = rotate_onto_z(normals[j])
+	path_points = np.transpose(np.dot(r, np.transpose(path_points)))
+	normals = np.transpose(np.dot(r, np.transpose(normals)))
+	euc = np.sqrt(np.dot(path_points[j + 1], path_points[j + 1]))
+	proj = np.sqrt(np.dot(path_points[j + 1][:2], path_points[j + 1][:2]))
+	coord += (euc / proj) * np.asarray([path_points[j + 1][0],path_points[j + 1][1]])
+	coords2 = np.vstack([coords2, coord])
+
+print(coords2)
+
+x = coords2[:, 0]
+y = coords2[:, 1]
+
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111)
+ax3.set_xlim(-5,5)
+ax3.set_ylim(-5,5)
+ax3.scatter(x, y)
+ax3.plot(x, y)
+fig3.savefig("2Dpath_2.png")
     
 '''
 p = [0., 0., -1.]
